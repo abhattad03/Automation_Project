5c4c0e9ac99597113ffe3fdc5737f45e3558d3f3
abhattad <abhattad@tibco.com>
2021-06-27 14:51:06 +0530

    Task 3

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 #This repository contains solution for task 2 and 3 of assignment required for upgrad PGSD DevOps Course
 #This repository contains shell scripts
+
+Task 2: It involved installation of Apache2 for Creating a WebServer. We create an automation script to install, check and keep the apache2 service running on the WebServer. We also archive the apache2 logs and upload it to the S3 bucket. Script named automation is created for it. It should be placed in /root/Automation_Project/ with. The script is to be given execution permission (chmod +x /root/Automation_Project/automation.sh) and run with root user privileges. This is end of Task 2 script. The version at this point is tagged Automation-v0.1
+
+Task 3: This involves in creating a Inventory of logs file and making it visible by inventory.html Also we need to make this automatically run daily by adding it to cron jobs. This is end of Task 3 script. The version at this point is tagged Automation-v0.2
--- automation.sh
+++ automation.sh
@@ -1,6 +1,8 @@
 sudo apt update -y
 
 echo "Check if apache2 is installed"
+
+############# Check if apache2 is installed#####################
 pkg="apache2"
 if dpkg -s  $pkg
 then
@@ -11,6 +13,8 @@ else
 fi
 #dpkg -s apache2
 echo "Check if apache2 is enabled"
+
+########### Check if apache2 is enabled #################################
 if systemctl status $pkg
 then
     echo "$pkg is running"
@@ -20,6 +24,7 @@ else
 fi
 
 echo "create tar file for logs"
+###############Create tar ###################
 
 timestamp=$(date '+%d%m%Y-%H%M%S')
 name="Aishwarya"
@@ -29,8 +34,53 @@ file="$name-httpd-logs-$timestamp"
     tar -cvf $file.tar /var/log/apache2/*.log
     mv $file.tar /tmp/
 
-
+############### Upload to S3 ####################################
 s3_bucket="upgrad-aishwarya"
 aws s3 \
 cp /tmp/$name-httpd-logs-$timestamp.tar \
 s3://$s3_bucket/$name-httpd-logs-$timestamp.tar
+
+
+#################### Here Task 2 Ends_Automation-v0.1 ############################################
+
+#################### Task 3 ####################################################
+
+# 4 - Updating the Inventory file with the latest log backup.
+
+
+if [ -f "/var/www/html/inventory.html" ]; 
+then
+	
+	printf "<p>" >> /var/www/html/inventory.html
+	printf "\n\t$(ls -lrth /tmp | grep httpd | cut -d ' ' -f 10 | cut -d '-' -f 2,3 | tail -1)" >> /var/www/html/inventory.html
+	printf "\t\t$(ls -lrth /tmp | grep httpd | cut -d ' ' -f 10 | cut -d '-' -f 4,5 | cut -d '.' -f 1 | tail -1)" >> /var/www/html/inventory.html
+	printf "\t\t\t $(ls -lrth /tmp | grep httpd | cut -d ' ' -f 10 | cut -d '-' -f 4,5 | cut -d '.' -f 2 | tail -1 )" >> /var/www/html/inventory.html
+	printf "\t\t\t\t$(ls -lrth /tmp/ | grep httpd | cut -d ' ' -f 6 | tail -1)" >> /var/www/html/inventory.html
+	printf "</p>" >> /var/www/html/inventory.html
+	
+else 
+	touch /var/www/html/inventory.html
+	printf "<p>" >> /var/www/html/inventory.html
+	printf "\tLog-Type\tDate-Created\tType\tSize" >> /var/www/html/inventory.html
+	printf "</p>" >> /var/www/html/inventory.html
+	printf "<p>" >> /var/www/html/inventory.html
+	printf "\n\t$(ls -lrth /tmp | grep httpd | cut -d ' ' -f 10 | cut -d '-' -f 2,3 | tail -1)" >> /var/www/html/inventory.html
+	printf "\t\t$(ls -lrth /tmp | grep httpd | cut -d ' ' -f 10 | cut -d '-' -f 4,5 | cut -d '.' -f 1 | tail -1)" >> /var/www/html/inventory.html
+	printf "\t\t\t $(ls -lrth /tmp | grep httpd | cut -d ' ' -f 10 | cut -d '-' -f 4,5 | cut -d '.' -f 2 | tail -1)" >> /var/www/html/inventory.html
+	printf "\t\t\t\t$(ls -lrth /tmp/ | grep httpd | cut -d ' ' -f 6 |tail -1)" >> /var/www/html/inventory.html
+	printf "</p>" >> /var/www/html/inventory.html
+	
+fi
+
+
+# 5 - Scheduling cronjob for Daily running of automation script
+
+if [ -f "/etc/cron.d/automation" ];
+then
+	echo "Automation script in place for Daily 00:00 hrs"
+else
+	touch /etc/cron.d/automation
+	printf "0 0 * * * root /root/Automation_Project/auotmation.sh" > /etc/cron.d/automation
+fi
+
+####################### Here Task 3 Ends_Automation-v0.2 ###################################################
